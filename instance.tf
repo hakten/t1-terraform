7d843fbc504d57d480e5b220fa68bc2a9ee3acec
Huseyin Akten <husakten@gmail.com>
2019-12-21 16:54:49 -0600

    more commands

--- instance.tf
+++ instance.tf
@@ -15,6 +15,11 @@ resource "aws_instance" "wp-project-instance" {
       inline = [
         "sudo yum install httpd -y",
         "sudo systemctl start httpd && sudo systemctl enable httpd"
+        "sudo yum install mariadb mariadb-server -y",
+        "sudo systemctl start mariadb && sudo systemctl enable mariadb",
+        "sudo yum update -y",
+        "sudo yum install epel-release -y",
+        "sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y",
         ]
       } 
   lifecycle{
