caee98328e2f6cf3db03df7073b82c093b7fcc64
Huseyin Akten <husakten@gmail.com>
2019-12-22 15:07:54 -0600

    sg edited

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "wp-project-instance-3" {
   instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
-  security_groups = ["ssh_only"]
+  security_groups = ["web_server"]
   provisioner "remote-exec" {
     connection {
       host        = self.public_ip
