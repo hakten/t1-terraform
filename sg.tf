7329d9cb3cb0c4e1bef5469d7040babd701f604f
Huseyin Akten <husakten@gmail.com>
2019-12-22 15:07:20 -0600

    sg is corrected

--- sg.tf
+++ sg.tf
@@ -1,11 +1,18 @@
-resource "aws_security_group" "ssh_only" { 
-  name        = "ssh_only" 
+resource "aws_security_group" "web_server" { 
+  name        = "web_server" 
   description = "allow ssh inbound traffic" 
   vpc_id      = "vpc-010b2c2926bc8ee6c" 
 
   ingress { 
     from_port   = 22 
     to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+
+} 
+ingress { 
+    from_port   = 80
+    to_port     = 80 
     protocol    = "tcp" 
     cidr_blocks = ["0.0.0.0/0"]   
 
