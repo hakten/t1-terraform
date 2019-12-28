41cf577e07085775d3b03223a8def7d58b59fcb2
Huseyin Akten <husakten@gmail.com>
2019-12-27 20:31:45 -0600

    deneme

--- sg.tf
+++ sg.tf
@@ -20,6 +20,32 @@ ingress {
   egress { 
     from_port       = 0 
     to_port         = 0 
+    protocol        = "-1" 
+    cidr_blocks     = ["0.0.0.0/0"] 
+  } 
+} 
+resource "aws_security_group" "ssh_deneme" { 
+  name        = "ssh_deneme" 
+  description = "allow ssh inbound traffic" 
+  vpc_id      = "vpc-010b2c2926bc8ee6c" 
+
+  ingress { 
+    from_port   = 22 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+
+} 
+ingress { 
+    from_port   = 80
+    to_port     = 80 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+
+} 
+  egress { 
+    from_port       = 0 
+    to_port         = 0 
     protocol        = "" 
     cidr_blocks     = ["0.0.0.0/0"] 
   } 
