dba10ccb1446c2f961ed711ad9ac7d8e9af500fc
Huseyin Akten <husakten@gmail.com>
2019-12-28 16:51:46 -0600

    userdata

--- instance.tf
+++ instance.tf
@@ -5,4 +5,5 @@ resource "aws_instance" "wp-project-instance-3" {
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["ssh_web"]
+  user_data = file("userdata")
 }
