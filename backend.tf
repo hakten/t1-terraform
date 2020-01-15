80ee2dbc022597026c5a62bdfa8735c62f43d061
Huseyin Akten <husakten@gmail.com>
2020-01-14 22:24:00 -0600

    switched to multi environment

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-tfstate-huseyin"
-    key    = "t1-terraform"
-    region = "us-east-1"
-  }
-}
---
+++ module-vpc/module/module-vpc-vard.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region        = "${var.region}"
+}
+
+terraform {
+    backend "s3" {}
+}
+
+
+module "vpc" {
+  source = "../."
+
+cidr            = "${var.cidr}"
+
+project         = "${var.project}"
+environment     = "${var.environment}"
+
+azs             = "${var.azs}"
+public_subnets  = "${var.public_subnets}"
+private_subnets = "${var.private_subnets}"
+
+}
+
+
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+
+output "vpc_cidr" {
+  value = "${module.vpc.vpc_cidr}"
+}
+
+output "public_subnets" {
+  value = "${module.vpc.public_subnets}"
+}
+
+output "private_subnets" {
+  value = "${module.vpc.private_subnets}"
+}
