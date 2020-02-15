18c7fd1edff73d3b2319b5eab03d5360415d34d1
Huseyin Akten <husakten@gmail.com>
2020-02-15 17:31:46 -0600

    webapp init commit

--- OLD/instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "wp-project-instance-3" {
-  count = var.count_instance
-  ami             = var.ami
-  instance_type   = var.instance_type
-  associate_public_ip_address = var.associate_public_ip_address
-  key_name = aws_key_pair.deployer.key_name
-  security_groups = ["ssh_web"]
-  user_data = file("userdata")
-}
--- OLD/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-ami             = "ami-02eac2c0129f6376b"
-instance_type   = "t2.micro"
-count_instance  = 1
-associate_public_ip_address  = "true"
-user            = "centos"
-ssh_key_location= "~/.ssh/id_rsa"
